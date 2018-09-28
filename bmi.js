7b7d79774bd5936cbd6fdd4a6da72cb8b0b9e98b
iibor <39728771+iibor@users.noreply.github.com>
2018-09-28 17:45:50 +0300

    muutoksia

--- bmi.js
+++ bmi.js
@@ -4,25 +4,26 @@ function bmi() {
   var BMI = (weight/Math.pow(height, 2)) * 10000;
   var tulos = BMI.toPrecision(3);
 
+
+
   if (tulos < 15) {
-    window.alert("Painoindeksisi on " + tulos +
-    " eli olet sairaalloisen alipainoinen");
+    document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos + " , olet sairaalloisen alipainoinen" + "</h3>";
   } else if (tulos >= 15 && tulos <= 17) {
-    window.alert("Painoindeksisi on " + tulos + " eli olet " +
-    "merkittävästi alipainoinen");
+  document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos +
+    ", eli olet merkittävästi alipainoinen" + "</h3>";
   } else if (tulos >= 17 && tulos <= 18.5) {
-    window.alert("Painoindeksisi on " + tulos + " eli sinulla on normaalia" +
-    "alhaisempi paino");
+  document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos +
+    ", sinulla on normaalia alhaisempi paino" + "</h3>";
   } else if (tulos >= 18.5 && tulos <= 25) {
-    window.alert("Painoindeksisi on " + tulos + " eli olet normaali painoinen");
+    document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos + " eli olet normaalipainoinen" + "</h3>";
   } else if (tulos >= 25 && tulos <= 30) {
-    window.alert("Painoindeksisi on " + tulos + " eli olet lievästi ylipainoinen");
+    document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos + " eli olet lievästi ylipainoinen" + "</h3>";
   } else if (tulos >= 30 && tulos <= 35) {
-    window.alert("Painoindeksisi on " + tulos + " eli olet merkittävästi ylipainoinen");
+    document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos + " eli olet merkittävästi ylipainoinen" + "</h3>";
   } else if (tulos >= 35 && tulos <= 40) {
-    window.alert("Painoindeksisi on " + tulos + " eli olet vaikeasti ylipainoinen");
+    document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos + " eli olet vaikeasti ylipainoinen"+ "</h3>";
   } else {
-    window.alert("Painoindeksisi on " + tulos + " eli olet sairaalloisen ylipainoinen");
+    document.getElementById('bmi').innerHTML = "<h3>" + "Painoindeksisi on " + tulos + " eli olet sairaalloisen ylipainoinen" + "</h3>";
   }
 
 
