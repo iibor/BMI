b5f36eb0780f35a646fbfb0b6c530471ab0ba1ee
iibor <39728771+iibor@users.noreply.github.com>
2018-09-29 14:18:25 +0300

    lisättiin lomakkeen kenttien tyhjennys

--- bmi.js
+++ bmi.js
@@ -13,27 +13,34 @@ function bmi() {
   } else {
 
 
-
+document.getElementById('bmi').innerHTML = "<h3>" + "Pituus: " + height + "cm" + "<br />" + "Paino: " + weight + "kg" + "</h3>" + "<br />";
 //käydään läpi elseif -lauseilla eri tulos-vaihtoehdot
   if (tulos < 15) {
-    document.getElementById('bmi').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos + " , olet sairaalloisen alipainoinen" + "</h3>";
+    document.getElementById('arvio').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos + " , olet sairaalloisen alipainoinen" + "</h3>";
   } else if (tulos >= 15 && tulos < 17.5) {
-  document.getElementById('bmi').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos +
+  document.getElementById('arvio').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos +
     ", eli olet merkittävästi alipainoinen" + "</h3>";
   } else if (tulos >= 17.5 && tulos < 19) {
-  document.getElementById('bmi').innerHTML = "<h3 id='lieva'>" + "Painoindeksisi on " + tulos +
+  document.getElementById('arvio').innerHTML = "<h3 id='lieva'>" + "Painoindeksisi on " + tulos +
     ", sinulla on normaalia alhaisempi paino" + "</h3>";
   } else if (tulos >= 19 && tulos < 26) {
-    document.getElementById('bmi').innerHTML = "<h3 id='normaali'>" + "Painoindeksisi on " + tulos + " eli olet normaalipainoinen" + "</h3>";
+    document.getElementById('arvio').innerHTML = "<h3 id='normaali'>" + "Painoindeksisi on " + tulos + " eli olet normaalipainoinen" + "</h3>";
   } else if (tulos >= 26 && tulos < 31) {
-    document.getElementById('bmi').innerHTML = "<h3 id='lieva'>" + "Painoindeksisi on " + tulos + " eli olet lievästi ylipainoinen" + "</h3>";
+    document.getElementById('arvio').innerHTML = "<h3 id='lieva'>" + "Painoindeksisi on " + tulos + " eli olet lievästi ylipainoinen" + "</h3>";
   } else if (tulos >= 31 && tulos < 36) {
-    document.getElementById('bmi').innerHTML = "<h3 id='lieva'>" + "Painoindeksisi on " + tulos + " eli olet merkittävästi ylipainoinen" + "</h3>";
+    document.getElementById('arvio').innerHTML = "<h3 id='lieva'>" + "Painoindeksisi on " + tulos + " eli olet merkittävästi ylipainoinen" + "</h3>";
   } else if (tulos >= 36 && tulos < 41) {
-    document.getElementById('bmi').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos + " eli olet vaikeasti ylipainoinen"+ "</h3>";
+    document.getElementById('arvio').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos + " eli olet vaikeasti ylipainoinen"+ "</h3>";
   } else {
-    document.getElementById('bmi').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos + " eli olet sairaalloisen ylipainoinen" + "</h3>";
+    document.getElementById('arvio').innerHTML = "<h3 id='sairas'>" + "Painoindeksisi on " + tulos + " eli olet sairaalloisen ylipainoinen" + "</h3>";
   }
+
+  document.getElementById('lomake').addEventListener('submit', function(laske) {
+    laske.preventDefault();
+
+  });
+
+  document.getElementById('lomake').reset();
 
 
 }
